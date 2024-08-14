70a0a7775d677f1f894565c8de4fbb306a66cac3
timfkl <timfkl11@gmail.com>
2024-08-14 13:30:21 +0100

    Implemented SpecialityRepository interface

--- DAL/SpecialityRepository.cs
+++ DAL/SpecialityRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,19 +11,81 @@ namespace DAL
 {
     public class SpecialityRepository : ISpecialityRepository
     {
-        public Task<Speciality> GetByIdAsync(int specialityId)
+        public async Task<Speciality> GetByIdAsync(int specialityId)
         {
-            throw new NotImplementedException();
+            var speciality = new Speciality();
+
+            var sql = new StringBuilder();
+            sql.AppendLine("SELECT * FROM speciality");
+            sql.AppendLine("WHERE Id = @specialityId");
+
+            await using (var connection = new MySqlConnection(Config.DbConnectionString))
+            {
+                await connection.OpenAsync();
+
+                var command = new MySqlCommand(sql.ToString(), connection);
+                command.Parameters.AddWithValue("specialityId", specialityId);
+
+                var reader = await command.ExecuteReaderAsync();
+                if (await reader.ReadAsync())
+                {
+                    speciality = PopulateSpeciality(reader);
+                }
+            }
+
+            return speciality;
         }
 
-        public Task<List<Speciality>> ListAllAsync()
+        public async Task<List<Speciality>> ListAllAsync()
         {
-            throw new NotImplementedException();
+            var specialityList = new List<Speciality>();
+
+            var sql = new StringBuilder();
+            sql.AppendLine("SELECT * FROM speciality");
+
+            await using (var connection = new MySqlConnection(Config.DbConnectionString))
+            {
+                await connection.OpenAsync();
+
+                var command = new MySqlCommand(sql.ToString(), connection);
+
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    specialityList.Add(PopulateSpeciality(reader));
+                }
+            }
+
+            return specialityList;
         }
 
-        public Task SaveAsync(Speciality speciality)
+        public async Task SaveAsync(Speciality speciality)
         {
-            throw new NotImplementedException();
+            var sql = new StringBuilder();
+            sql.AppendLine("UPDATE speciality SET");
+            sql.AppendLine("SpecialityName = @specialityName,");
+            sql.AppendLine("WHERE Id = @specialityId");
+
+            await using (var connection = new MySqlConnection(Config.DbConnectionString))
+            {
+                await connection.OpenAsync();
+
+                var command = new MySqlCommand(sql.ToString(), connection);
+                command.Parameters.AddWithValue("specialityName", speciality.SpecialityName);
+                command.Parameters.AddWithValue("specialityId", speciality.Id);
+
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        private Speciality PopulateSpeciality(IDataRecord data)
+        {
+            var speciality = new Speciality
+            {
+                Id = int.Parse(data["Id"].ToString()),
+                SpecialityName = data["SpecialityName"].ToString(),
+            };
+            return speciality;
         }
     }
 }
