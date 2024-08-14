15ac5c9ef7708b818c6fa3983a8844b38162b6d2
timfkl <timfkl11@gmail.com>
2024-08-14 18:56:26 +0100

    Fixed bug for display Specialities tab

--- FullStackTechTest/Program.cs
+++ FullStackTechTest/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 // DI
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
 builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+builder.Services.AddScoped<ISpecialityRepository, SpecialityRepository>();
+builder.Services.AddScoped<IPersonSpecialitiesRepository,PersonSpecialitiesRepository>();
 
 var app = builder.Build();
 
