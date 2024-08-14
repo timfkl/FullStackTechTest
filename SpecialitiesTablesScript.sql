a76f18641faefee8ddcaa570a96b3346558a844a
timfkl <timfkl11@gmail.com>
2024-08-14 13:06:45 +0100

    Changed script to reflect renames

--- SpecialitiesTablesScript.sql
+++ SpecialitiesTablesScript.sql
@@ -1,14 +1,14 @@
-CREATE TABLE PeopleSpecialities(
+CREATE TABLE PersonSpecialities(
 	Id INT PRIMARY KEY auto_increment,
     PersonId INT NOT NULL,
     SpecialityId INT NOT NULL
 );
 
-CREATE TABLE Specialities(
+CREATE TABLE Speciality(
 	Id INT PRIMARY KEY auto_increment,
     SpecialityName VARCHAR(32)
 );
 
-INSERT INTO Specialities (SpecialityName) VALUES ('Anaesthetics'), ('Cardiology'), ('Dermatology'),
+INSERT INTO Speciality (SpecialityName) VALUES ('Anaesthetics'), ('Cardiology'), ('Dermatology'),
 ('Emergency Medicine'), ('General Practice (GP)'), ('Neurology'), ('Obstetrics and Gynaecology'),
 ('Ophthalmology'), ('Orthopaedic Surgery'), ('Psychiatry');
