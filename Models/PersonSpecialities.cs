7d76ecb3de69b27c1ca1801d0863c159f658a89b
timfkl <timfkl11@gmail.com>
2024-08-14 18:57:30 +0100

    Added optional parameter in order to more easily display SpecialityName

--- Models/PersonSpecialities.cs
+++ Models/PersonSpecialities.cs
@@ -1,9 +1,13 @@
-﻿namespace Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
 {
     public class PersonSpecialities
     {
         public int Id { get; set; }
         public int PersonId { get; set; }
         public int SpecialityId {  get; set; }
+        [NotMapped]
+        public string SpecialityName { get; set; } = string.Empty;
     }
 }
