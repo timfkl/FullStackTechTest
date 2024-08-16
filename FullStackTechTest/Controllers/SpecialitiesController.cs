415529648f0f7be909f7bdd49eb89833594c887f
timfkl <timfkl11@gmail.com>
2024-08-16 18:47:17 +0100

    Worked on Delete Specialities button

--- FullStackTechTest/Controllers/SpecialitiesController.cs
+++ FullStackTechTest/Controllers/SpecialitiesController.cs
@@ -47,4 +47,11 @@ public class SpecialitiesController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete (int id, [FromForm] DetailsViewModel model)
+    {
+        await _specialityRepository.DeleteAsync(model.Speciality);
+        return RedirectToAction("Index");
+    }
 }
