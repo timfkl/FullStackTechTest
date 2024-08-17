1988ea3e3ef62d15b6564404e31b651d4b8fe652
timfkl <timfkl11@gmail.com>
2024-08-17 14:50:58 +0100

    Worked on back end logic for database

--- FullStackTechTest/Controllers/ImportController.cs
+++ FullStackTechTest/Controllers/ImportController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using DAL;
+using Models;
+using Microsoft.AspNetCore.Mvc;
+using FullStackTechTest.Models.Import;
+using FullStackTechTest.Models.Shared;
 
 namespace FullStackTechTest.Controllers
 {
@@ -7,6 +12,10 @@ namespace FullStackTechTest.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Create(List<Person> people, List<Address> address) {
+            
         }
     }
 }
