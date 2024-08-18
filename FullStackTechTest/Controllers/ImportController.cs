3c03967aa4e4f912df26d9eac5e404a62cf71342
timfkl <timfkl11@gmail.com>
2024-08-18 01:25:49 +0100

    Worked on import JSON

--- FullStackTechTest/Controllers/ImportController.cs
+++ FullStackTechTest/Controllers/ImportController.cs
@@ -9,13 +9,39 @@ namespace FullStackTechTest.Controllers
 {
     public class ImportController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IPersonRepository _personRepository;
+        private readonly IAddressRepository _addressRepository;
+
+        public ImportController(ILogger<HomeController> logger, IPersonRepository personRepository, IAddressRepository addressRepository)
+        {
+            _logger = logger;
+            _personRepository = personRepository;
+            _addressRepository = addressRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public async Task<IActionResult> Create(List<Person> people, List<Address> address) {
+        public async Task<List<Person>> Create(List<Person> people, List<Address> address) {
+
+            List<Person> removedPeople = new List<Person>();
+
+            foreach (var person in people)
+            {
+                var gmcIsPresent = await _personRepository.CheckGMCAsync(person.GMC);
+                if (gmcIsPresent)
+                {
+                    removedPeople.Add(person);
+                    people.Remove(person);
+                }
+            }
+            await _personRepository.ImportAsync(people);
             
+            //return table with Import values
+
         }
     }
 }
